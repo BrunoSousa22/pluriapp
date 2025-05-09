10d3bf3faa87a824581a6142b0b585904ec6cfda
BrunoSousa22 <brunosousa.9@hotmail.com>
2025-05-09 13:05:49 +0100

    Update pluriapp.py

--- pluriapp.py
+++ pluriapp.py
@@ -8,11 +8,3 @@ st.title("🚀 CLICAAAAAA!")
 # Button
 if st.button("Enviar balões"):
     st.balloons()
-
-# File upload
-uploaded_file = st.file_uploader("Faça upload de um arquivo CSV", type="csv")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.subheader("Visualização do CSV:")
-    st.dataframe(df)
-    
