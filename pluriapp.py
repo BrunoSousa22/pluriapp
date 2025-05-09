2f05f8ac5019f07ce426619db23f9aa478875e40
BrunoSousa22 <brunosousa.9@hotmail.com>
2025-05-09 13:08:53 +0100

    Update pluriapp.py

--- pluriapp.py
+++ pluriapp.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 # Page title
 st.set_page_config(page_title="Demo App", layout="centered")
-st.title("🚀 CLICAAAAAA!")
+st.title("🚀 Bem-vindo!")
 
 # Button
-if st.button("Enviar balões"):
-    st.balloons()
+if st.button("Clicar aqui para uma surpresa"):
+    resposta = st.radio("Tens a certeza que estás pronto?", ["Sim", "Não"], index=None)
+
+    if resposta == "Sim":
+        resposta1 = st.radio("Certeza certezinha?", ["Sim", "Não"], index=None)
+        if resposta1 == "Sim":
+            st.balloons()
+        elif resposta1 == "Não":
+            st.write("Nem sabes o que perdes")
+
+    elif resposta == "Não":
+        st.write("Nem sabes o que perdes")
