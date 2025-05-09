6e026c29b2a91a3733b42de9c33006659074edcf
BrunoSousa22 <brunosousa.9@hotmail.com>
2025-05-09 12:40:55 +0100

    pluriapp.py

--- pluriapp.py
+++ pluriapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Page title
 st.set_page_config(page_title="Demo App", layout="centered")
-st.title("🚀 BOOOOOOOAS :D !")
+st.title("🚀 A Equipa de Stocks é a melhor!")
 
 # Text input
 name = st.text_input("Insira o seu nome:")
